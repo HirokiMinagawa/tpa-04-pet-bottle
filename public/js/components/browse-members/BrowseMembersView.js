165ba57215c022b943ad6948bbb595be8d0f4729
hiroki minagawa <hiroki6805@gmail.com>
2018-11-29 23:51:52 +0900

    DOM操作を追加

--- public/js/components/browse-members/BrowseMembersView.js
+++ public/js/components/browse-members/BrowseMembersView.js
@@ -26,6 +26,47 @@ class BrowseMembersView {
 
     this.clearDom();
 
+    const memberProfile = document.createElement('div');
+    memberProfile.className = 'member-profile';
+
+    const memberImageBox = document.createElement('div');
+    memberImageBox.className = 'member-image-box';
+
+    const memberImage = document.createElement('img');
+    memberImage.setAttribute('src', thumbnailUrl);
+    memberImage.setAttribute('alt', '(会員のイメージ)');
+
+    memberImageBox.appendChild(memberImage);
+
+    const h3First = document.createElement('h3');
+    h3First.textContent = '名前';
+
+    const memberName = document.createElement('p');
+    memberName.className = 'member-name';
+    memberName.textContent = name;
+
+    const h3Second = document.createElement('h3');
+    h3Second.textContent = '一言';
+
+    const memberText = document.createElement('p');
+    memberText.className = 'member-text';
+    memberText.textContent = quote;
+
+    const button = document.createElement('button');
+    button.className = 'btn-next-member';
+    button.textContent = '次へ';
+    button.onclick = handleBrowseNextMember;
+
+    memberProfile.appendChild(memberImageBox);
+    memberProfile.appendChild(h3First);
+    memberProfile.appendChild(memberName);
+    memberProfile.appendChild(h3Second);
+    memberProfile.appendChild(memberText);
+
+    const container = document.querySelector('#container');
+    container.appendChild(memberProfile);
+    container.appendChild(button);
+
     /*
       TODO: 動的にDOM要素を作成して、
       "#container"というidを持つDIV要素の子要素として追加してください...
